49fc48f09dcf06c4b9af89a5e26bf9ecdadc0161
Nemanja Savic <savic1996.neca@gmail.com>
2026-01-19 00:06:00 +0100

    Register verification added

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,23 +1,45 @@
+import { useForm, type FieldValues } from "react-hook-form"
+
+interface RegisterFormData{
+  firstname: string,
+  lastname: string,
+  username: string,
+  email: string,
+  password: string
+}
 
 function Register() {
+  const { register, handleSubmit, formState: {errors}} = useForm<RegisterFormData>();
+  const onSubmit = (data : FieldValues) => console.log(data);
   return (
-    
     <>
       <div className="flex flex-col justify-center items-center h-full bg-blue-200">
         <h1 className="text-3xl font-bold mb-10">Register your account</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            
           <legend className="fieldset-legend">Your details</legend>
           <label className="label">Firstname</label>
-          <input type="text" className="input" placeholder="Firstname" />
+          <input type="text" className="input" placeholder="Firstname" {...register("firstname", {required: true})}/>
+          {errors.firstname?.type === "required" && <p className="text-xs text-error">Firstname is required</p>}
+
           <label className="label">Lastname</label>
-          <input type="text" className="input" placeholder="Lastname" />
+          <input type="text" className="input" placeholder="Lastname" {...register("lastname", {required: true})} />
+          {errors.lastname?.type === "required" && <p className="text-xs text-error">Lastname is required</p>}
+
           <label className="label">Username</label>
-          <input type="text" className="input" placeholder="Username" />
+          <input type="text" className="input" placeholder="Username" {...register("username", {required: true})}/>
+          {errors.username?.type === "required" && <p className="text-xs text-error">Username is required</p>}
+
           <label className="label">Email</label>
-          <input type="text" className="input" placeholder="Email" />
+          <input type="email" placeholder="Email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} className="input"/>
+          {errors.email?.type === "required" && <p className="text-xs text-error">Email is required</p>}
+          {errors.email?.type === "pattern" && <p className="text-xs text-error">Enter valid email address</p>}
+
           <label className="label">Password</label>
-          <input type="text" className="input" placeholder="Password" />
+          <input type="password" placeholder="Password" {...register("password", {required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d).{5,}$/})} className="input" />
+          {errors.password && <p className="text-xs text-error">Password is required <br></br>Needs to have at least 1 character and 1 number <br></br>Needs to be at least 5 length</p>}
+
           <button className="btn btn-neutral mt-4">Register</button>
           </fieldset>
         </form>
