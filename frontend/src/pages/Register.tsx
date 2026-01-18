775e0d381f51c73b74a8e8c3be71d20992178414
Nemanja Savic <savic1996.neca@gmail.com>
2026-01-18 22:27:10 +0100

    Register UI made

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,28 @@
 
 function Register() {
   return (
-    <div>Register</div>
+    
+    <>
+      <div className="flex flex-col justify-center items-center h-full bg-blue-200">
+        <h1 className="text-3xl font-bold mb-10">Register your account</h1>
+        <form>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Your details</legend>
+          <label className="label">Firstname</label>
+          <input type="text" className="input" placeholder="Firstname" />
+          <label className="label">Lastname</label>
+          <input type="text" className="input" placeholder="Lastname" />
+          <label className="label">Username</label>
+          <input type="text" className="input" placeholder="Username" />
+          <label className="label">Email</label>
+          <input type="text" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="text" className="input" placeholder="Password" />
+          <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+        </form>
+      </div>
+    </>
   )
 }
 
--- frontend/src/routing/router.tsx
+++ frontend/src/routing/router.tsx
@@ -3,8 +3,7 @@ import Login from "../pages/Login"
 import Register from "../pages/Register"
 
 const router = createBrowserRouter([
-    { path: "/", element: <div>Hello World</div>},
-    { path: "/login", element: <Login />},
+    { path: "/", element: <Login />},
     { path: "/register", element: <Register />}
 ])
 
